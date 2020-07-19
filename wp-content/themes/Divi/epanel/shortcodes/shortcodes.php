414e0e35187397b57baec6b6fde8c243098605fd
DaveParky3 <46577085+DaveParky3@users.noreply.github.com>
2020-07-19 23:28:17 +0100

    2nd - all wp-content

---
+++ wp-content/themes/Divi/epanel/shortcodes/shortcodes.php
@@ -0,0 +1,8 @@
+<br />
+<font size='1'><table class='xdebug-error xe-uncaught-exception' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Fatal error: Uncaught Error: Call to undefined function get_template_directory_uri() in /Users/Davide/Sites/www.digitalwave.dev.cc/wp-content/themes/Divi/epanel/shortcodes/shortcodes.php on line <i>6</i></th></tr>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Error: Call to undefined function get_template_directory_uri() in /Users/Davide/Sites/www.digitalwave.dev.cc/wp-content/themes/Divi/epanel/shortcodes/shortcodes.php on line <i>6</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0036</td><td bgcolor='#eeeeec' align='right'>737664</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/Users/Davide/Sites/www.digitalwave.dev.cc/wp-content/themes/Divi/epanel/shortcodes/shortcodes.php' bgcolor='#eeeeec'>.../shortcodes.php<b>:</b>0</td></tr>
+</table></font>
---
+++ wp-content/themes/Divi/includes/builder/module/Comments.php
@@ -0,0 +1,8 @@
+<br />
+<font size='1'><table class='xdebug-error xe-uncaught-exception' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Fatal error: Uncaught Error: Class 'ET_Builder_Module' not found in /Users/Davide/Sites/www.digitalwave.dev.cc/wp-content/themes/Divi/includes/builder/module/Comments.php on line <i>3</i></th></tr>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Error: Class 'ET_Builder_Module' not found in /Users/Davide/Sites/www.digitalwave.dev.cc/wp-content/themes/Divi/includes/builder/module/Comments.php on line <i>3</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0372</td><td bgcolor='#eeeeec' align='right'>586368</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/Users/Davide/Sites/www.digitalwave.dev.cc/wp-content/themes/Divi/includes/builder/module/Comments.php' bgcolor='#eeeeec'>.../Comments.php<b>:</b>0</td></tr>
+</table></font>
